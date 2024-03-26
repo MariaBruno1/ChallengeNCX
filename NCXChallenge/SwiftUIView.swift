f7a914ad53b8c73ac6555787388d3a3df8713936
Maria Bruno <mariabruno@10-20-56-231.0200.fed.it.iosda.org>
2024-03-26 08:48:30 +0100

    main more

--- NCXChallenge/SwiftUIView.swift
+++ NCXChallenge/SwiftUIView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+                Text("Tab Content 1")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(hue: 1.0, saturation: 0.386, brightness: 0.847, opacity: 0.828))
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }.tag(1)
+                Text("Tab Content 2").tabItem {
+                    Image(systemName: "figure.run")
+                    Text("Fitness")
+                }.tag(2)
+                Text("Tab Content 3").tabItem {
+                    Image(systemName: "fork.knife.circle")
+                    Text("Diet")
+                }
+                Text("Tab Content 4").tabItem {
+                    Image(systemName: "cooktop")
+                    Text("Recipes")
+                }
+           }
+        .accentColor(Color.black)
+        }
     }
-}
 
-#Preview {
-    SwiftUIView()
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
 }
