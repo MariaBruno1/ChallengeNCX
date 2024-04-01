cf6b6613361b2a6ed014bc89c33da36d1599e7b6
Maria Bruno <mariabruno@MacBook-Pro-27.homenet.telecomitalia.it>
2024-04-01 22:34:54 +0200

    calendar

---
+++ NCXChallenge/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  NCXChallenge
+//
+//  Created by Maria Bruno on 01/04/24.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
